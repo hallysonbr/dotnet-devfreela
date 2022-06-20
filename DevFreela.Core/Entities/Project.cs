50e8194002644306afc2947835e3afd2be4d99ec
Hallyson Bruno <hallyson.bruno@hotmail.com>
2022-06-20 17:52:05 -0300

    Criando a camada Application

--- DevFreela.Core/Entities/Project.cs
+++ DevFreela.Core/Entities/Project.cs
@@ -9,7 +9,7 @@ namespace DevFreela.Core.Entities
 {
     public class Project : BaseEntity
     {
-        public Project(string title, string description, int idClient, int idFreelancer, decimal? totalCost)
+        public Project(string title, string description, int idClient, int idFreelancer, decimal totalCost)
         {
             Title = title;
             Description = description;
@@ -26,11 +26,17 @@ namespace DevFreela.Core.Entities
         public string Description { get; private set; }
         public int IdClient { get; set; }
         public int IdFreelancer { get; private set; }
-        public decimal? TotalCost { get; private set; }
+        public decimal TotalCost { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime? StartedAt { get; private set; }
         public DateTime? FinishedAt { get; private set; }
-        public ProjectStatusEnum Status { get; set; }
+        public ProjectStatusEnum Status { get; private set; }
         public List<ProjectComment> Comments { get; private set; }
+
+        public void Cancel()
+        {
+            if (Status == ProjectStatusEnum.InProgress)
+                Status = ProjectStatusEnum.Cancelled;
+        }
     }
 }
