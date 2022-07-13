4eb8210a543e0b70ee8677af78e0d66efab0ba2d
Hallyson Bruno <hallyson.bruno@hotmail.com>
2022-07-13 16:03:16 -0300

    Gerando Migrations e Persistindo com Dapper.

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,8 +1,12 @@
-﻿using DevFreela.Application.InputModels;
+﻿using Dapper;
+using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +18,18 @@ namespace DevFreela.Application.Services.Implementations
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
 
-        public ProjectService(DevFreelaDbContext dbContext)
+        public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration )
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
         public int Create(NewProjectInputModel inputModel)
         {
             var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdClient, inputModel.IdFreelancer, inputModel.TotalCost);
             _dbContext.Projects.Add(project);
+            _dbContext.SaveChanges();
             return project.Id;
         }
 
@@ -30,18 +37,21 @@ namespace DevFreela.Application.Services.Implementations
         {
             var comment = new ProjectComment(inputModel.Content, inputModel.IdProject, inputModel.IdUser);
             _dbContext.ProjectComments.Add(comment);
+            _dbContext.SaveChanges();
         }
 
         public void Delete(int id)
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Cancel();
+            _dbContext.SaveChanges();
         }
 
         public void Finish(int id)
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Finish();
+            _dbContext.SaveChanges();
         }
 
         public List<ProjectViewModel> GetAll(string query)
@@ -53,7 +63,9 @@ namespace DevFreela.Application.Services.Implementations
 
         public ProjectDetailsViewModel GetById(int id)
         {
-            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            var project = _dbContext.Projects.Include(p => p.Client)
+                                             .Include(p => p.Freelancer)
+                                             .SingleOrDefault(p => p.Id == id);
 
             if (project is null) return null;
 
@@ -63,7 +75,9 @@ namespace DevFreela.Application.Services.Implementations
                 project.Description,
                 project.TotalCost,
                 project.CreatedAt,
-                project.FinishedAt
+                project.FinishedAt,
+                project.Client.FullName,
+                project.Freelancer.FullName
                 );
             return projectDetailsViewModel;
         }
@@ -72,12 +86,22 @@ namespace DevFreela.Application.Services.Implementations
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Start();
+            //_dbContext.SaveChanges();
+
+            //Usando Dapper
+            using (var sqlconnection = new SqlConnection(_connectionString))
+            {
+                sqlconnection.Open();
+                var script = "UPDATE Projects SET status = @status, StartedAt = @startedat WHERE Id = @id";
+                sqlconnection.Execute(script, new { status = project.Status, startedat = project.StartedAt, id = project.Id });
+            }
         }
 
         public void Update(UpdateProjectInputModel inputModel)
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == inputModel.Id);
             project.Update(inputModel.Title, inputModel.Description, inputModel.TotalCost);
+            _dbContext.SaveChanges();
         }
     }
 }
